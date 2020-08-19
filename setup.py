1970075d1ad490cdfc436ed510a7b1d9a41a7a65
Andrew Palmer <atp@sdf.org>
2020-08-18 19:18:27 -0500

    Update setup.py for distribution

--- setup.py
+++ setup.py
@@ -3,5 +3,16 @@ from setuptools import setup, Extension
 
 setup(
     name='cstring',
-    ext_modules=[Extension('cstring', sources=['src/cstring.c'])]
+    version='0.1.0',
+    author='Andrew Palmer',
+    author_email='atp@sdf.org',
+    description='String implementation based on UTF-8 C-strings',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/atpalmer-python/python-cstring',
+    ext_modules=[Extension('cstring', sources=['src/cstring.c'])],
+    classifiers=[
+    ],
+    python_requires='>=3.6',
 )
+
